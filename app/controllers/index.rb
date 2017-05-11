7514ff39540a4ae83f640c0c5d946a60f870062a
Nick Devlin <nrdevlin@gmail.com>
2017-05-11 18:54:50 -0500

    Add heatmap iframe, pitch sorting

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,54 +1,37 @@
 require 'json'
-require 'heatmap'
 require 'plotly'
-
-# get '/' do
-#   @data = JSON.parse(open('lester.json').read)
-#   map = Heatmap::Map.new
-#   @coordinates = []
-
-#   @data.each do |point|
-#     if point["location_x"] && point["location_x"] > -5
-#       map << Heatmap::Area.new(point["location_x"] + 3, point["location_z"] + 3)
-#       @coordinates << [point["location_x"] + 3, point["location_z"] + 3]
-#     end
-#   end
-
-#   map.output('test.png')
-
-#   erb :index
-# end
-
-# data = open('lester.json').read
-# map  = Heatmap.new
-
-# data.each do |point|
-#   point = JSON.parse(point)
-#   map << Heatmap::Area.new(*point.values)
-# end
-
-# map.output('large.png')
+require_relative '../helpers/pitch_helper'
 
 get '/' do
 
-plotly = PlotLy.new('nrdevlin', 'zZpuShG5TQlXGT0VonC9')
+  @pitches = PitchHelper.pitches_with_location.max_by { |pitch| pitch["location_z"] }
 
-data = { z: [[1, 20, 1], [1, 30, 1], [1, 20, 1]] }
+  plotly = PlotLy.new('nrdevlin', PLOTLY_API_KEY)
 
-    args = {
-      filename: 'ruby_test_heat_map',
-      fileopt: 'overwrite',
-      style: { type: 'contour' },
-      layout: {
-        title: 'Dorky Fun'
-      },
-      world_readable: true
-    }
+  data = {
+          x: [-2, -1, 0, 1, 2],
+          y: [0, 1, 2],
+          z: [[0, 20, 1, 6, 8], [0, 30, 1, 7, 25], [0, 12, 1, 20, 1]]
+        }
 
-    plotly.plot(data, args) do |response|
-      @url = response['url']
-    end
+  args = {
+    filename: 'ruby_test_heat_map',
+    fileopt: 'overwrite',
+    style: { type: 'heatmap' },
+    layout: {
+      title: 'Jon Lester Heatmap'
+    },
+    world_readable: true
+  }
 
+  plotly.plot(data, args) do |response|
+  end
 
-
+  erb :index
 end
+
+#location_x min: -2.754
+#location_x max: 3.023
+
+#location_z min: -1.654
+#location_z max: 5.391
